4f054b42a41dbaab3086597d1ddde7378a4a54f1
Sahil Khanna <sahilkhanna.kg7@gmail.com>
2022-03-14 01:17:52 +0530

    Initial Commit

---
+++ test0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(test0.Startup))]
+namespace test0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
